efc813aad1ad1706adf3a2cd32bef5b802b48c8e
mahitha72 <mahitha29.ikkurthi@gmail.com>
2024-08-02 19:31:43 +0530

    hii

---
+++ samplejs.js
@@ -0,0 +1,12 @@
+const t=document.getElementById("hi");
+t.innerHTML="Hello World!!";
+
+const v=document.getElementsByClassName("h");
+v.textContent="hello";
+
+document.querySelector("#anil").textContent="welcome to vv";
+const z=document.querySelectorAll(".a");
+z.innerHTML="<h2>GOOD EVENING</h2>";
+const con=document.createElement('names');
+con.innerHTML="SRKR"
+document.body.appendChild(con);
